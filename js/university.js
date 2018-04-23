e1da84f239836d3c4f131928b95f841043b97179
kschwieterman <trinasch@hotmail.com>
2018-04-23 10:07:45 -0700

    added logo images and js files for solar and about page content

---
+++ js/university.js
@@ -0,0 +1,62 @@
+var university = [
+  {
+    "website": "http://www.ufl.edu/",
+    "name": "University of Florida (UF)",
+    "logo": "/ufl-logo.jpg",
+  },
+  {
+    "website": "http://www.fsu.edu/",
+    "name": "Florida State University (FSU)",
+    "logo": "/fsu-logo.jpg",
+  },
+  {
+    "website": "http://www.ucf.edu/",
+    "name": "University of Central Florida (UCF)",
+    "logo": "/ucf-logo.jpg",
+  },
+  {
+    "website": "http://www.usf.edu/",
+    "name": "University of South Florida (USF)",
+    "logo": "/usf-logo.jpg",
+  },
+  {
+    "website": "http://www.famu.edu/",
+    "name": "Florida A&M University (FAMU)",
+    "logo": "/famu-logo.jpg",
+  },
+  {
+    "website": "http://www.fau.edu/",
+    "name": "Florida Atlanta University (FAU)",
+    "logo": "/fau-logo.jpg",
+  },
+  {
+    "website": "http://www.fgcu.edu/",
+    "name": "Florida Gulf Coast University (FGCU)",
+    "logo": "/fgcu-logo.jpg",
+  },
+  {
+    "website": "http://www.fiu.edu/",
+    "name": "Florida International University (FIU)",
+    "logo": "/fiu-logo.jpg",
+  },
+  {
+    "website": "http://www.ncf.edu/",
+    "name": "New College of Florida (NCF)",
+    "logo": "/ncf-logo.jpg",
+  },
+  {
+    "website": "http://www.unf.edu/",
+    "name": "University of North Florida (UNF)",
+    "logo": "/unf-logo.jpg",
+  },
+  {
+    "website": "http://www.uwf.edu/",
+    "name": "University of West Florida (UWF)",
+    "logo": "/uwf-logo.jpg",
+  },
+  {
+    "website": "http://floridapolytechnic.org/",
+    "name": "Florida Polytechnic University (FPU)",
+    "logo": "/fpu-logo.jpg",
+  },
+]
