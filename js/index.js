c5a6aea1a8859152ed5cec10af21e88fd8616d79
kschwieterman <trinasch@hotmail.com>
2018-06-11 19:31:39 -0700

    added to do for securing api key

--- js/index.js
+++ js/index.js
@@ -44,6 +44,7 @@ function getProducedData(){
   let request = new XMLHttpRequest()
   
   // URL to data goes here
+  //TO DO: secure key
   let requestUrl = "https://api.eia.gov/series/?api_key=f4d8118ecdf14870ca176c9bd8b25d34&series_id=SEDS.REPRB.FL.A"
   
   // Open a connection
@@ -88,6 +89,7 @@ function getElectricConsumed(){
   let request = new XMLHttpRequest()
   
   //URL to data goes here
+  //TO DO: secure key
   let requestUrl = "http://api.eia.gov/series/?api_key=f4d8118ecdf14870ca176c9bd8b25d34&series_id=SEDS.ESTCB.FL.A"
   
   //Open a connection
